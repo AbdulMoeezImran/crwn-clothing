21a490d5772fe4027f372faddc332d74301005d2
AbdulMoeezImran <moeezimran6@gmail.com>
2023-05-02 08:06:10 +0500

    Adding cart context and shopping icon as well as cart dropdown

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import Home from './routes/home/home.components';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import { Routes, Route } from 'react-router-dom';
-
-
-const Shop = () => {
-  return <h1>I am the Shop Page</h1>;
-}
+import Shop from './routes/shop/shop.component';
 
 
 const App = () => {
--- src/routes/navigation/navigation.component.js
+++ src/routes/navigation/navigation.component.js
@@ -1,14 +1,19 @@
 import { Fragment, useContext } from 'react';
 import { UserContext } from "../../contexts/user.contexts";
+import { CartContext } from "../../contexts/cart.context";
 import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.component';
 import './navigation.style.scss';
 
 
 const Navigaiton = () => {
 
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
+
 
     return (
         <Fragment>
@@ -20,11 +25,13 @@ const Navigaiton = () => {
                     <Link className='nav-link' to='/shop'>
                         SHOP
                     </Link>
-                    {currentUser ? (<span className='nav-link' onClick={signOutUser} >Sign Out</span>) : (<Link className='nav-link' to='/auth'>
-                        Sign In
+                    {currentUser ? (<span className='nav-link' onClick={signOutUser} >SIGN OUT</span>) : (<Link className='nav-link' to='/auth'>
+                        SIGN IN
                     </Link>)}
-
+                    <CartIcon/>
                 </div>
+                {isCartOpen && <CartDropDown/>}
+                
             </div>
             <Outlet />
         </Fragment>
